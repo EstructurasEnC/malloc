dd26beaa1a9eaac01c9cf4fc51470d20b80d954a
Root <zigmaxcontacto@gmail.com>
2018-08-30 21:01:11 -0500

    Inicio de una estructura

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=1,b=0;
+
+    if(a==1 && b==1){
+        printf("los dons son iguales \n");
+
+    }else{
+        printf("son diferentes \n");
+    }
+    return 0;
+}
